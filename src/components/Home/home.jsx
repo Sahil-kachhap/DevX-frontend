1fbd0383a0bfa566453c717f3be6aa2d227a8fe4
Sahil Kachhap <Sahil.kachhap111989@gmail.com>
2023-04-25 16:19:47 +0530

    initial home screen setup

---
+++ src/components/Home/home.jsx
@@ -0,0 +1,51 @@
+import { Box, Button, Heading, HStack, Image, Stack, Text, VStack } from '@chakra-ui/react'
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './home.css'
+import vg from "../../assets/images/logo.jpg";
+import {CgGoogle, CgYoutube} from "react-icons/cg";
+import {SiCoursera, SiUdemy} from "react-icons/si";
+import {DiAws} from "react-icons/di";
+
+const Home = () => {
+  return <section className='home'>
+      <div className='container'>
+         <Stack 
+           direction={["column", "row"]}
+           height="100%"
+           justifyContent={["center","space-between"]}
+           alignItems="center"
+           spacing={['16', '56']}
+         >
+            <VStack width={'full'}  alignItems={['center', 'flex-end']}>
+                <Heading children="LEARN FROM THE EXPERTS" size={'2xl'} />
+                <Text children="Find Valuable content at reasonable price"/>
+                <Link to="/courses">
+                    <Button size={"lg"} colorScheme="yellow">Explore Now</Button>
+                </Link> 
+            </VStack> 
+            {/* <Image className='vector-graphics' boxSize={'md'} src={vg} objectFit="contain"/> */}
+         </Stack>
+      </div>
+      
+
+
+
+      <Box  padding={'8'} bg="blackAlpha.800">
+          <Heading textAlign={'center'} fontFamily="body" color={'yellow.400'} children="OUR BRANDS"/>
+          <HStack className='brandsBanner' justifyContent={'space-evenly'} marginTop="4">
+              <CgGoogle />
+              <CgYoutube />
+              <SiCoursera />
+              <SiUdemy />
+              <DiAws />
+          </HStack>
+      </Box>
+
+      <div className='container2'>
+          <video autoPlay controls  controlsList='nodownload nofullscreen noremoteplayback' disablePictureInPicture disableRemotePlayback></video>
+      </div>
+  </section>
+}
+
+export default Home
