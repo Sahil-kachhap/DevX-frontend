f2bf0f88865ca616010e715293745a1ae95a8fe6
Sahil Kachhap <Sahil.kachhap111989@gmail.com>
2023-05-13 11:25:43 +0530

    feat: created request course and contact us pages

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
 import ForgetPassword from './components/Auth/ForgetPassword';
 import ResetPassword from './components/Auth/ResetPassword';
+import Contact from './components/Contact/contact';
+import Request from './components/Request/request';
 
 function App() {
   return <Router>
@@ -16,6 +18,8 @@ function App() {
       <Route path='/' element={<Home/>}/>
       <Route path='/courses' element={<Courses/>}/>
       <Route path='/login' element={<Login/>}/>
+      <Route path='/contact' element={<Contact />} />
+      <Route path='/request' element={<Request />} />
       <Route path='/register' element={<Register />} />
       <Route path='/forgetpassword' element={<ForgetPassword />} />
       <Route path='/resetpassword/:token' element={<ResetPassword />}/>
